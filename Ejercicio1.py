3ecae559270ce6881cb1373a2980b7bdca158a39
Karla Figueroa <karla.figueroa@upb.edu.co>
2025-08-21 11:35:11 -0500

    Ejercicio1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -14,3 +14,18 @@ altura = float(altura)
 imc = peso/altura**2
 #Mostrar imc
 print("Tu IMC = ", imc)
+if 18.5 <= imc <= 24.9:
+    print("Clasificación normal")
+else:
+    if 25 <= imc <= 29.9:
+        print("Clasificación sobre peso")
+    else :
+        if 30 <= imc <= 34.9:
+            print("Clasificación Obesidad I")
+        else:
+            if 35 <= imc <= 39.9:
+                print("Clasificación Obesidad II")
+            else:
+                if imc >= 40:
+                    print("Clasificación Obesidad Extrema III")
+                    
