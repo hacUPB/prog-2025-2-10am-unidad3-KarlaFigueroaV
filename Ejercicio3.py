58e1070f53bcc56c08dd967c977ccf734ff7f3a7
Karla Figueroa <karla.figueroa@upb.edu.co>
2025-09-02 10:16:22 -0500

    .

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,5 +1,6 @@
 #Ejercicio 3
-#Se le pide al usuario que ingrese un numero entero y que muestre un mensaje si el numero es divisible por 3.
+##Condicional Simple
+###Se le pide al usuario que ingrese un numero entero y que muestre un mensaje si el numero es divisible por 3.
 
 numero = int(input("Ingrese un número:"))
 residuo = numero % 3
@@ -7,4 +8,5 @@ residuo = numero % 3
 if residuo == 0:
     print(numero, "Divisible entre 3")
 
+##Condicional doble
 
--- RETO/RETO.py
+++ RETO/RETO.py
@@ -2,20 +2,20 @@
 ## Por: Karla Figueroa Velilla y Santiago Duarte Cueto
 
 print("=== MENÚ PRINCIPAL ===")
-print("1. Problema 1")
-print("2. Problema 2")
-print("3. Problema 3")
+print("1. Límite de ángulo de ataque")
+print("2. Peso y balance")
+print("3. Plan de vuelo con reserva de combustible")
 print("4. Salir")
 
 opcion = int(input("Selecciona una opción: "))
 
 match opcion:
     case 1:
-        print("Problema 1")
+        print("Límite de ángulo de ataque")
     case 2:
-        print("Problema 2")
+        print("Peso y balance")
     case 3:
-        print("Problema 3")
+        print("Plan de vuelo con reserva de combustible")
     case 4:
         print("Saliendo del programa...")
     case _:
